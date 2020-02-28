fdb490eb3ab0cef155a69783026f861bbc72b448
maxschaefer-fau <60692026+maxschaefer-fau@users.noreply.github.com>
2020-02-28 15:52:16 +0100

    Add files via upload

--- 2020_SebVR/1_RoomModel/fct_excite_cont.m
+++ 2020_SebVR/1_RoomModel/fct_excite_cont.m
@@ -9,20 +9,26 @@ lam = 2*pi*100;
 ft = scaling*exp(a*t).*sin(lam*t);
 
 % spatial exciation
-x0 = 1.3;
-x1 = 3.7;
+x0 = 4;
+x1 = 2;
 y1 = 3;
-y0 = 1;
+y0 = 3;
 
 l = norm([x0;y0] - [x1;y1]);
 
 lamb = 3*pi;
 
+% Normal vector 
+nx = 1/l*(y1 - y0);
+ny = 1/l*(x0 - x1);
+
 % Sebastian: I corrected to y1 - y0 in the second cos
-fun = @(xi,lx, ly) cos(lx*(x0 + xi*(x1 -x0))).*...
-    cos(ly*(y0 + xi*(y1 - y0))).*sin(lamb*xi);
+% fun = @(xi,lx, ly) cos(lx*(x0 + xi*(x1 -x0))).*...
+%     cos(ly*(y0 + xi*(y1 - y0))).*sin(lamb*xi);
 
-
+fun = @(xi,lx, ly) (nx*lx*sin(lx*(x0 + xi*(x1 -x0))).*cos(ly*(y0 + xi*(y1 - y0))) ...
+        + ny*ly*cos(lx*(x0 + xi*(x1 -x0))).*sin(ly*(y0 + xi*(y1 - y0)))...
+        ).*sin(lamb*xi);
 
 for mu = 1:ftm.Mu
     lx = ftm.lambdaX(mu);
@@ -30,6 +36,6 @@ for mu = 1:ftm.Mu
     
     foo = integral(@(xi) fun(xi,lx,ly),0, 1);
     
-    excite(mu,:) = foo*l*ft;
+    excite(mu,:) = foo*l*ft*(-4/(room.rho*conj(ftm.smu(mu))));
 end
 end
